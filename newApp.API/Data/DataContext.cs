b5a261106582afa2471e39aceea29d64224f0dac
ryan.wilson <ryan.wilson@ctalk.co.uk>
2018-12-04 16:08:02 +0000

    Initial Commit

---
+++ newApp.API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using newApp.API.Models;
+
+namespace newApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+
+    }
+}
