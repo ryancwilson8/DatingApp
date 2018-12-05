b97b55fa1b1fb2ba98bcd9804b3f40329c0d1d2d
ryan.wilson <ryan.wilson@ctalk.co.uk>
2018-12-05 14:03:46 +0000

    Push to WFH.

--- newApp.API/Data/DataContext.cs
+++ newApp.API/Data/DataContext.cs
@@ -8,10 +8,8 @@ namespace newApp.API.Data
         public DataContext(DbContextOptions<DataContext> options) : base (options) {
 
         }
-
         public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }
-
-
+        
     }
 }
--- newApp.API/Data/IAuthRepository.cs
+++ newApp.API/Data/IAuthRepository.cs
@@ -5,8 +5,8 @@ namespace newApp.API.Data
 {
     public interface IAuthRepository
     {
-         Task<User> Register(User user, string password);
-         Task<User> Login(string username, string password);
+        Task<User> Register(User user, string password);
+        Task<User> Login(string username, string password);
 
         Task<bool> UserExists(string username);
     }
