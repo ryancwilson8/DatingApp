9b9ed2b8ae851bc2c6db839ada3889f8131ddb33
ryan.wilson <ryan.wilson@ctalk.co.uk>
2018-12-06 16:29:09 +0000

    Merge to WFH.

---
+++ newApp-SPA/src/app/_services/alertify.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+declare let alertify: any;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertifyService {
+
+  constructor() { }
+
+  confirm (message: string, okCallback: () => any) {
+    alertify.confirm(message, function(e) {
+      if (e) {
+        okCallback();
+      } else { }
+    });
+  }
+
+}
